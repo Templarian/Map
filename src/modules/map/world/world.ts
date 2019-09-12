92a99f97e5cdc6f39262490d8c7d39c255853642
Austin Andrews <admin@templarian.com>
2019-09-11 20:54:15 -0500

    Progress.

--- src/modules/map/world/world.ts
+++ src/modules/map/world/world.ts
@@ -3,19 +3,22 @@ import { LightningElement } from 'lwc';
 export default class World extends LightningElement {
 
   renderedCallback() {
-    const div = this.template.childNodes[1] as HTMLElement;
-    div.style.left = `${Math.floor((window.innerWidth - 64) / 2)}px`;
-    div.style.top = `${Math.floor((window.innerHeight - 64) / 2)}px`;
     window.addEventListener('resize', this.resize.bind(this));
     this.resize();
   }
 
   resize() {
+    // Map
     const div = this.template.childNodes[1] as HTMLElement;
+    div.style.left = `${Math.floor((window.innerWidth - 64) / 2)}px`;
+    div.style.top = `${Math.floor((window.innerHeight - 64) / 2)}px`;
+    // Background Dots
     var rect = div.getBoundingClientRect();
     const host = this.template.host as HTMLElement;
-    host.style.backgroundPositionX = `${rect.left % 64}px`;
-    host.style.backgroundPositionY = `${rect.top % 64}px`;
+    const x = Math.floor((rect.left + window.scrollX) % 64);
+    const y = Math.floor((rect.top + window.scrollY) % 64);
+    host.style.backgroundPositionX = `${x}px`;
+    host.style.backgroundPositionY = `${y}px`;
   }
 
 }
